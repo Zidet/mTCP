2df36119b8dfe526e81a47cab19edae041be02fe
kkkio <kevin.l.yide@gmail.com>
2017-03-15 21:35:14 +0800

    update global vars

---
+++ Main/client/circular_queue.h
@@ -0,0 +1,30 @@
+#ifndef __CIRCULAR_QUEUE__
+
+#define __CIRCULAR_QUEUE__
+
+
+#define BUFFSIZE 1024*8
+
+typedef myqueue{
+  int front,rear;
+  char buffer[BUFFSIZE];
+}QUEUE;
+
+QUEUE* createqueue();
+
+int isempty(struct queue*q);
+
+int isfull(QUEUE*q);
+
+int enqueue(QUEUE *q, char data);
+
+int dequeue(QUEUE *q);
+
+//  Manipulate send buffer using enenqueue
+int writeSendBuff(QUEUE *q, const char* data);
+
+
+// Wipe out data from the beginning
+int wipeSendBuff(QUEUE *q, int length);
+
+#endif
