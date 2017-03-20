a1b5f68be75b6d08d5a868497af339a5bbdb9fb3
Daniel <zhangsibin1996@gmail.com>
2017-03-20 13:20:46 +0800

    new server

--- Main/server/mtcp_server.c
+++ Main/server/mtcp_server.c
@@ -61,13 +61,13 @@ void mtcp_accept(int socket_fd, struct sockaddr_in *server_addr){
 int mtcp_read(int socket_fd, unsigned char *buf, int buf_len){
     // check whether buf is empty
     /*
-    if (strlen(buf)==0){
-      pthread_cond_wait(&app_thread_sig, &app_thread_sig_mutex);
-    }
-    else{
-      fprintf(stderr, "Buffer is not empty\n");
-    }
-    */
+       if (strlen(buf)==0){
+       pthread_cond_wait(&app_thread_sig, &app_thread_sig_mutex);
+       }
+       else{
+       fprintf(stderr, "Buffer is not empty\n");
+       }
+       */
     //change state to data transmission
     pthread_mutex_lock(&info_mutex);
     state=2;
@@ -102,46 +102,51 @@ static void *send_thread(){
     int32_t shutdown = 0;
     //int32_t local_seq;
     while(!shutdown){
-      mTCPPacket* packet = (mTCPPacket*) malloc(sizeof(mTCPPacket));
-      mTCPHeader header;
-      //sleep
-      pthread_cond_wait(&send_thread_sig, &send_thread_sig_mutex);
-      //check state
-      pthread_mutex_lock(&info_mutex);
-      //local_seq = SEQ;
-      pthread_mutex_unlock(&info_mutex);
-      if (state==1){
-        header=pack_header(mTCP_SYN_ACK,0);
-        packet->header=header;
-        memset(packet->buffer, 0, 1000);
-        sendto(sfd, (void*)packet, sizeof(packet), 0, (struct sockaddr*)dest_addr,
-                sizeof(*dest_addr));
-      }
-      else if(state==2){
-        // send ACK packet out
-        local_ack=SEQ+200;
-        header=pack_header(mTCP_ACK, local_ack);
-        packet->header=header;
+        mTCPPacket* packet = (mTCPPacket*) malloc(sizeof(mTCPPacket));
+        mTCPHeader header;
+        //sleep
+        pthread_cond_wait(&send_thread_sig, &send_thread_sig_mutex);
+        //check state
         pthread_mutex_lock(&info_mutex);
-        ACK=SEQ+200;
-        memset(packet->buffer, 0, 1000);
+        //local_seq = SEQ;
         pthread_mutex_unlock(&info_mutex);
-        sendto(sfd, (void*)packet, sizeof(packet), 0, (struct sockaddr*)dest_addr,
-                sizeof(*dest_addr));
-        //wake up application thread
-        pthread_mutex_lock(&app_thread_sig_mutex);
-        pthread_cond_signal(&app_thread_sig);
-        pthread_mutex_unlock(&app_thread_sig_mutex);
-      }
-      else if(state==3){
-        header=pack_header(mTCP_FIN_ACK, 0);
-        packet->header=header;
-        memset(packet->buffer, 0, 1000);
-        sendto(sfd, (void*)packet, sizeof(packet), 0, (struct sockaddr*)dest_addr,
-                sizeof(*dest_addr));
-        shutdown=1;
-      }
-      free(packet);
+        printf("[SERVER] Send Thread: state = %d\n", state);
+        if (state==1){
+            header=pack_header(mTCP_SYN_ACK,0);
+            packet->header=header;
+            memset(packet->buffer, 0, 1000);
+            sendto(sfd, (void*)packet, sizeof(packet), 0, (struct sockaddr*)dest_addr,
+                    sizeof(*dest_addr));
+            printf("[SERVER] Send Thread: SYN_ACK sent\n");
+        }
+        else if(state==2){
+            // send ACK packet out
+            local_ack=SEQ+200;
+            header=pack_header(mTCP_ACK, local_ack);
+            packet->header=header;
+            pthread_mutex_lock(&info_mutex);
+            ACK=SEQ+200;
+            memset(packet->buffer, 0, 1000);
+            pthread_mutex_unlock(&info_mutex);
+            printf("[SERVER] Send Thread: ACK = %d\n", local_ack);
+            sendto(sfd, (void*)packet, sizeof(packet), 0, (struct sockaddr*)dest_addr,
+                    sizeof(*dest_addr));
+            printf("[SERVER] Send Thread: ACK sent\n");
+            //wake up application thread
+            pthread_mutex_lock(&app_thread_sig_mutex);
+            pthread_cond_signal(&app_thread_sig);
+            pthread_mutex_unlock(&app_thread_sig_mutex);
+        }
+        else if(state==3){
+            header=pack_header(mTCP_FIN_ACK, 0);
+            packet->header=header;
+            memset(packet->buffer, 0, 1000);
+            sendto(sfd, (void*)packet, sizeof(packet), 0, (struct sockaddr*)dest_addr,
+                    sizeof(*dest_addr));
+            shutdown=1;
+            printf("[SERVER] Send Thread: FIN-ACK sent\n");
+        }
+        free(packet);
     }
     pthread_exit(0);
 }
@@ -153,77 +158,82 @@ static void *receive_thread(){
     mTCPHeader header = 0;
     int32_t type = -1; int32_t rest = -1;
     while(!shutdown){
-      //mTCPPacket *received = (mTCPPacket*)malloc(sizeof(mTCPPacket));
-      int32_t length;
-      socklen_t fromlen=sizeof(*dest_addr);
-      length = recvfrom(sfd,  buf, MAX_BUF_SIZE,0,
-              (struct sockaddr *)dest_addr, &fromlen);
-      if(length <= 0){
-          continue;
-          fprintf(stderr,"Error on receiving data\n");
+        printf("[SERVER] Receive Thread: state = %d\n", state);
+        //mTCPPacket *received = (mTCPPacket*)malloc(sizeof(mTCPPacket));
+        int32_t length;
+        socklen_t fromlen=sizeof(*dest_addr);
+        length = recvfrom(sfd,  buf, MAX_BUF_SIZE,0,
+                (struct sockaddr *)dest_addr, &fromlen);
+        if(length <= 0){
+            continue;
+            fprintf(stderr,"Error on receiving data\n");
         }
         memcpy(&header,buf,4);
         memcpy(&buff,buf, strlen(buf));
         unpack_header(&header, &type, &rest);
-        printf("%d\n", type);
         pthread_mutex_lock(&info_mutex);
         lastreceive = type;
         ACK=rest;
         //local_seq=SEQ;
         pthread_mutex_unlock(&info_mutex);
-            if(state == -1){
-                fprintf(stderr,"State not updated I bet");
-            }
-            if(state == 1){ // 3-way handshake
-                if(type == mTCP_SYN){
-                //wake up send thread
-                    pthread_mutex_lock(&send_thread_sig_mutex);
-                    pthread_cond_signal(&send_thread_sig);
-                    pthread_mutex_unlock(&send_thread_sig_mutex);
-                }
-                else if(type==mTCP_ACK){
-                //wake up app thread to return
-                    pthread_mutex_lock(&send_thread_sig_mutex);
-                    pthread_cond_signal(&app_thread_sig);
-                    pthread_mutex_unlock(&send_thread_sig_mutex);
-                    printf("[SERVER] Receive Thread: 3-way ok\n");
-                }
-                else{
-                    fprintf(stderr,"Error on 3-way handshake at server\n");
-                }
-            }
-            if(state == 2){ // data transmission
-                //printf("state = %d\n",state);
-                if(type == mTCP_DATA){
-                    pthread_mutex_lock(&info_mutex);
-                    SEQ=ACK;
-                    pthread_mutex_unlock(&info_mutex);
-                    //wake up sending thread
-                    pthread_mutex_lock(&send_thread_sig_mutex);
-                    pthread_cond_signal(&send_thread_sig);
-                    pthread_mutex_unlock(&send_thread_sig_mutex);
-                }
-                else{
-                    fprintf(stderr,"Error on data transmission at server\n");
-                }
-            }
-            if(state == 3){ // 4-way handshake
-                if(type == mTCP_FIN){
-                    //wake up send thread
-                    pthread_mutex_lock(&send_thread_sig_mutex);
-                    pthread_cond_signal(&send_thread_sig);
-                    pthread_mutex_unlock(&send_thread_sig_mutex);
-                }
-                if(type == mTCP_ACK){
-                //wake up application thread
-                    pthread_mutex_lock(&app_thread_sig_mutex);
-                    pthread_cond_signal(&app_thread_sig);
-                    pthread_mutex_unlock(&app_thread_sig_mutex);
-                    //terminate itself
-                    shutdown=1;
-                }
-            }
-            //free(received);
+        if(state == -1){
+            fprintf(stderr,"State not updated I bet");
         }
-        pthread_exit(0);
+        if(state == 1){ // 3-way handshake
+            if(type == mTCP_SYN){
+                //wake up send thread
+                printf("[SERVER] Receive Thread: SYN received\n");
+                pthread_mutex_lock(&send_thread_sig_mutex);
+                pthread_cond_signal(&send_thread_sig);
+                pthread_mutex_unlock(&send_thread_sig_mutex);
+            }
+            else if(type==mTCP_ACK){
+                //wake up app thread to return
+                printf("[SERVER] Receive Thread: ACK received\n");
+                pthread_mutex_lock(&send_thread_sig_mutex);
+                pthread_cond_signal(&app_thread_sig);
+                pthread_mutex_unlock(&send_thread_sig_mutex);
+                printf("[SERVER] Receive Thread: 3-way ok\n");
+            }
+            else{
+                fprintf(stderr,"Error on 3-way handshake at server\n");
+            }
+        }
+        if(state == 2){ // data transmission
+            //printf("state = %d\n",state);
+            if(type == mTCP_DATA){
+                printf("[SERVER] Receive Thread: data received\n");
+                pthread_mutex_lock(&info_mutex);
+                SEQ=ACK;
+                pthread_mutex_unlock(&info_mutex);
+                //wake up sending thread
+                pthread_mutex_lock(&send_thread_sig_mutex);
+                pthread_cond_signal(&send_thread_sig);
+                pthread_mutex_unlock(&send_thread_sig_mutex);
+            }
+            else{
+                fprintf(stderr,"Error on data transmission at server\n");
+            }
+        }
+        if(state == 3){ // 4-way handshake
+            if(type == mTCP_FIN){
+                //wake up send thread
+                printf("[SERVER] Receive Thread: fin received\n");
+                pthread_mutex_lock(&send_thread_sig_mutex);
+                pthread_cond_signal(&send_thread_sig);
+                pthread_mutex_unlock(&send_thread_sig_mutex);
+            }
+            if(type == mTCP_ACK){
+                //wake up application thread
+                printf("[SERVER] Receive Thread: ACK received\n");
+                pthread_mutex_lock(&app_thread_sig_mutex);
+                pthread_cond_signal(&app_thread_sig);
+                pthread_mutex_unlock(&app_thread_sig_mutex);
+                //terminate itself
+                shutdown=1;
+            }
+        }
+        //free(received);
+    }
+    pthread_exit(0);
 }
